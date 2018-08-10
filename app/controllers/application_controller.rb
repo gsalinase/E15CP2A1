ae23079fe4e09714f819f96760b617499dffe45f
Gonzalo Salinas <gsalinase91@gmail.com>
2018-08-10 00:42:26 -0400

    Fix problem in authenticate_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+
+  def filter_admin!
+    authenticate_user!
+    redirect_to root_path, alert: "No tienes acceso" unless current_user.admin?
+  end
 end
--- db/migrate/20180810010153_add_name_and_username_and_admin_to_users.rb
+++ db/migrate/20180810010153_add_name_and_username_and_admin_to_users.rb
@@ -2,6 +2,6 @@ class AddNameAndUsernameAndAdminToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :name, :string
     add_column :users, :username, :string
-    add_column :users, :admin, :boolean
+    add_column :users, :admin, :boolean, default: false
   end
 end
